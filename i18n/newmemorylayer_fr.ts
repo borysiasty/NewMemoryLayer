a6bf96188281815cdfba8635af951577aa8f280f
Borys Jurgiel <info@borysjurgiel.pl>
2013-11-24 12:39:42 +0100

    v0.4.2: French translation by Pierre-Luc Auclair

---
+++ i18n/newmemorylayer_fr.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>NewMemoryLayer</name>
+    <message>
+        <location filename="../ui_newmemorylayer.ui" line="14"/>
+        <source>NewMemoryLayer</source>
+        <translation>Nouvelle couche temporaire</translation>
+    </message>
+    <message>
+        <location filename="../ui_newmemorylayer.ui" line="22"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../ui_newmemorylayer.ui" line="32"/>
+        <source>Memory layer</source>
+        <translation>Couche temporaire</translation>
+    </message>
+    <message>
+        <location filename="../ui_newmemorylayer.ui" line="49"/>
+        <source>Point</source>
+        <translation>Point</translation>
+    </message>
+    <message>
+        <location filename="../ui_newmemorylayer.ui" line="62"/>
+        <source>Line</source>
+        <translation>Ligne</translation>
+    </message>
+    <message>
+        <location filename="../ui_newmemorylayer.ui" line="75"/>
+        <source>Polygon</source>
+        <translation>Polygone</translation>
+    </message>
+    <message>
+        <location filename="../newmemorylayer.py" line="47"/>
+        <source>New Memory Layer</source>
+        <translation>Nouvelle couche temporaire</translation>
+    </message>
+    <message>
+        <location filename="../newmemorylayer.py" line="46"/>
+        <source>New Memory Layer...</source>
+        <translation>Nouvelle couche temporaire...</translation>
+    </message>
+</context>
+</TS>
